c93fc2322c652f5371dc8a2b467f5222df90fbb9
PabloSzx <pablosaez1995@gmail.com>
2018-11-21 12:43:42 -0300

    mensaje de salida

--- tarea/ETT.java
+++ tarea/ETT.java
@@ -8,7 +8,7 @@ public class ETT{
     public static void main(String[] args) {
         try {
             parser p = new parser(new Lexer(new BufferedReader(new InputStreamReader(System.in))));
-            System.out.println("Comenzando ETT para serie de instrucciones, ingrese \"editar\" para comenzar:");
+            System.out.println("Bienvenido/a a la consola ETT, ingrese \"editar\" para comenzar, y luego \"termino\" para cerrar el programa:");
             p.parse();      
         } catch (Exception e) {
             /* do cleanup here -- possibly rethrow e */
