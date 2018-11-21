e94c09d7dfaedddd394d45a16bf99d8420894898
PabloSzx <pablosaez1995@gmail.com>
2018-11-21 12:22:14 -0300

    mensaje de bienvenida

--- tarea/ETT.java
+++ tarea/ETT.java
@@ -8,6 +8,7 @@ public class ETT{
     public static void main(String[] args) {
         try {
             parser p = new parser(new Lexer(new BufferedReader(new InputStreamReader(System.in))));
+            System.out.println("Comenzando ETT para serie de instrucciones, ingrese \"editar\" para comenzar:");
             p.parse();      
         } catch (Exception e) {
             /* do cleanup here -- possibly rethrow e */
